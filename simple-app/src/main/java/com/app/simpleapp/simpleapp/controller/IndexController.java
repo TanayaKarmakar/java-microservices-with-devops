cc36dd5601fc9f916eaef3d86169940695471846
TanayaKarmakar <karmakar.tanaya@gmail.com>
2021-03-09 05:34:19 +0530

    updtae

--- simple-app/src/main/java/com/app/simpleapp/simpleapp/controller/IndexController.java
+++ simple-app/src/main/java/com/app/simpleapp/simpleapp/controller/IndexController.java
@@ -1,12 +1,17 @@
 package com.app.simpleapp.simpleapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
+    private Logger logger = LoggerFactory.getLogger(IndexController.class);
+
     @GetMapping("/hello")
     public String sayHello() {
+        logger.info("Accessing the IndexController");
         return "Hello World";
     }
 }
